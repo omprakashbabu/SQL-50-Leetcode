cbac6df1171f7f72eab8c039fd02e58e3d59bed0
omprakashbabu <116138027+omprakashbabu@users.noreply.github.com>
2025-08-03 13:21:28 +0530

    Update Leetcode_1148.sql

--- Leetcode_1148.sql
+++ Leetcode_1148.sql
@@ -1,13 +1,5 @@
 1148. Article Views I
-Solved
-Easy
-Topics
-premium lock icon
-Companies
-SQL Schema
-Pandas Schema
 Table: Views
-
 +---------------+---------+
 | Column Name   | Type    |
 +---------------+---------+
@@ -20,17 +12,11 @@ There is no primary key (column with unique values) for this table, the table ma
 Each row of this table indicates that some viewer viewed an article (written by some author) on some date. 
 Note that equal author_id and viewer_id indicate the same person.
  
-
 Write a solution to find all the authors that viewed at least one of their own articles.
-
 Return the result table sorted by id in ascending order.
-
 The result format is in the following example.
 
- 
-
 Example 1:
-
 Input: 
 Views table:
 +------------+-----------+-----------+------------+
