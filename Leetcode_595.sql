da027dfce742ec8add105333cec125132fc083ee
omprakashbabu <116138027+omprakashbabu@users.noreply.github.com>
2025-08-03 13:03:28 +0530

    Update Leetcode_595.sql

--- Leetcode_595.sql
+++ Leetcode_595.sql
@@ -1,11 +1,4 @@
 595. Big Countries
-Solved
-Easy
-Topics
-premium lock icon
-Companies
-SQL Schema
-Pandas Schema
 Table: World
 
 +-------------+---------+
@@ -20,21 +13,15 @@ Table: World
 name is the primary key (column with unique values) for this table.
 Each row of this table gives information about the name of a country, the continent to which it belongs, its area, the population, and its GDP value.
  
-
 A country is big if:
-
 it has an area of at least three million (i.e., 3000000 km2), or
 it has a population of at least twenty-five million (i.e., 25000000).
 Write a solution to find the name, population, and area of the big countries.
 
 Return the result table in any order.
-
 The result format is in the following example.
-
  
-
 Example 1:
-
 Input: 
 World table:
 +-------------+-----------+---------+------------+--------------+
