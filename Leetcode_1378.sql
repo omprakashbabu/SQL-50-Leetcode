14c3d99091dd90fe7a6a0f399011ce3832c8f52d
omprakashbabu <116138027+omprakashbabu@users.noreply.github.com>
2025-08-03 13:49:27 +0530

    Update Leetcode_1378.sql

--- Leetcode_1378.sql
+++ Leetcode_1378.sql
@@ -1,13 +1,5 @@
 -- 1378. Replace Employee ID With The Unique Identifier
--- Solved
--- Easy
--- Topics
--- premium lock icon
--- Companies
--- SQL Schema
--- Pandas Schema
 -- Table: Employees
-
 -- +---------------+---------+
 -- | Column Name   | Type    |
 -- +---------------+---------+
@@ -16,10 +8,8 @@
 -- +---------------+---------+
 -- id is the primary key (column with unique values) for this table.
 -- Each row of this table contains the id and the name of an employee in a company.
- 
 
 -- Table: EmployeeUNI
-
 -- +---------------+---------+
 -- | Column Name   | Type    |
 -- +---------------+---------+
@@ -29,17 +19,11 @@
 -- (id, unique_id) is the primary key (combination of columns with unique values) for this table.
 -- Each row of this table contains the id and the corresponding unique id of an employee in the company.
  
-
 -- Write a solution to show the unique ID of each user, If a user does not have a unique ID replace just show null.
-
 -- Return the result table in any order.
-
 -- The result format is in the following example.
 
- 
-
 -- Example 1:
-
 -- Input: 
 -- Employees table:
 -- +----+----------+
