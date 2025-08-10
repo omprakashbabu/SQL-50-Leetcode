8be3e78b60eccdf90a446368b991e57d82df2228
omprakashbabu <116138027+omprakashbabu@users.noreply.github.com>
2025-08-10 23:55:28 +0530

    Update Leetcode_1581.sql

--- Leetcode_1581.sql
+++ Leetcode_1581.sql
@@ -64,3 +64,15 @@
 -- Customer with id = 54 visited the mall three times. During 2 visits they did not make any transactions, and during one visit they made 3 transactions.
 -- Customer with id = 96 visited the mall once and did not make any transactions.
 -- As we can see, users with IDs 30 and 96 visited the mall one time without making any transactions. Also, user 54 visited the mall twice and did not make any transactions.
+
+
+SELECT v.customer_id, COUNT(*) AS count_no_trans
+FROM Visits v
+LEFT JOIN Transactions t
+  ON v.visit_id = t.visit_id
+WHERE t.transaction_id IS NULL
+GROUP BY v.customer_id;
+
+
+
+
