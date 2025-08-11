89eadd7d34a3ca01007115e9216512424bd07ed6
omprakashbabu <116138027+omprakashbabu@users.noreply.github.com>
2025-08-11 23:59:54 +0530

    Update Leetcode_1661.sql

--- Leetcode_1661.sql
+++ Leetcode_1661.sql
@@ -19,21 +19,13 @@
 -- The 'start' timestamp will always be before the 'end' timestamp for every (machine_id, process_id) pair.
 -- It is guaranteed that each (machine_id, process_id) pair has a 'start' and 'end' timestamp.
  
-
 -- There is a factory website that has several machines each running the same number of processes. Write a solution to find the average time each machine takes to complete a process.
-
 -- The time to complete a process is the 'end' timestamp minus the 'start' timestamp. The average time is calculated by the total time to complete every process on the machine divided by the number of processes that were run.
-
 -- The resulting table should have the machine_id along with the average time as processing_time, which should be rounded to 3 decimal places.
-
 -- Return the result table in any order.
-
 -- The result format is in the following example.
 
- 
-
 -- Example 1:
-
 -- Input: 
 -- Activity table:
 -- +------------+------------+---------------+-----------+
