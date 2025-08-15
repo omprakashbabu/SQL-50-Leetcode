0c9d289180cfb52c1a67086cd3dc702e32d155e0
omprakashbabu <116138027+omprakashbabu@users.noreply.github.com>
2025-08-15 23:19:14 +0530

    Update Leetcode_620.sql

--- Leetcode_620.sql
+++ Leetcode_620.sql
@@ -1,13 +1,5 @@
 620. Not Boring Movies
-Solved
-Easy
-Topics
-premium lock icon
-Companies
-SQL Schema
-Pandas Schema
 Table: Cinema
-
 +----------------+----------+
 | Column Name    | Type     |
 +----------------+----------+
@@ -19,18 +11,12 @@ Table: Cinema
 id is the primary key (column with unique values) for this table.
 Each row contains information about the name of a movie, its genre, and its rating.
 rating is a 2 decimal places float in the range [0, 10]
- 
 
 Write a solution to report the movies with an odd-numbered ID and a description that is not "boring".
-
 Return the result table ordered by rating in descending order.
-
 The result format is in the following example.
 
- 
-
 Example 1:
-
 Input: 
 Cinema table:
 +----+------------+-------------+--------+
