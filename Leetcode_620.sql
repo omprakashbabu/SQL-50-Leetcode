6295b09294a04d21a3ba6a71f2f33f9583b1ba7e
omprakashbabu <116138027+omprakashbabu@users.noreply.github.com>
2025-08-15 23:19:45 +0530

    Update Leetcode_620.sql

--- Leetcode_620.sql
+++ Leetcode_620.sql
@@ -37,3 +37,11 @@
 -- +----+------------+-------------+--------+
 -- Explanation: 
 -- We have three movies with odd-numbered IDs: 1, 3, and 5. The movie with ID = 3 is boring so we do not include it in the answer.
+
+-- Write your MySQL query statement below
+select * from cinema
+where id % 2 = 1 and description != 'boring'
+order by rating desc
+
+
+
