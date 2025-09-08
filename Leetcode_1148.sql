5a340dd245cf36bc4f1f1486954ae1c65d701614
Omprakash B <116138027+omprakashbabu@users.noreply.github.com>
2025-09-08 16:14:08 +0530

    Update Leetcode_1148.sql

--- Leetcode_1148.sql
+++ Leetcode_1148.sql
@@ -12,7 +12,7 @@
 -- Each row of this table indicates that some viewer viewed an article (written by some author) on some date. 
 -- Note that equal author_id and viewer_id indicate the same person.
  
--- Write a solution to find all the authors that viewed at least one of their own articles.
+-- Write a solution to find all the authors that viewed at least one of their own articles. 
 -- Return the result table sorted by id in ascending order.
 -- The result format is in the following example.
 
