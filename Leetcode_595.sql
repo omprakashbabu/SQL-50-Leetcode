58ed6c0e0568aaa8b01cb8c617da08c3ab74cc4a
Omprakash B <116138027+omprakashbabu@users.noreply.github.com>
2025-09-11 23:43:34 +0530

    Update Leetcode_595.sql

--- Leetcode_595.sql
+++ Leetcode_595.sql
@@ -10,7 +10,7 @@
 -- | population  | int     |
 -- | gdp         | bigint  |
 -- +-------------+---------+
--- name is the primary key (column with unique values) for this table.
+-- name is the primary key (column with unique values) for this table.  
 -- Each row of this table gives information about the name of a country, the continent to which it belongs, its area, the population, and its GDP value.
  
 -- A country is big if:
